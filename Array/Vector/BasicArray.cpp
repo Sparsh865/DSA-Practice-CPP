b723a84744d145e61b19b1d63d876a58b2c63589
Sparsh865 <sparsh865.be22@chitkara.edu.in>
2024-05-31 13:03:19 +0530

    basic array

---
+++ Array/Vector/BasicArray.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Largest Element
+//  int main(){
+//      int size;
+//      cout<<"Size of the array: ";
+//      cin>>size;
+//      int arr[size];
+//      for(int i=0;i<size;i++){
+//          cin>>arr[i];
+//      }
+//      sort(arr,arr+size);
+//      cout<<"Largest Element: "<<arr[size-1];
+//      return 0;
+//  }
+
+// K Largest
+//  int main()
+//  {
+//      int size;
+//      cout << "Size of the array: ";
+//      cin >> size;
+//      int arr[size];
+//      for (int i = 0; i < size; i++)
+//      {
+//          cin >> arr[i];
+//      }
+//      cout<<"K=";
+//      int k;
+//      cin>>k;
+//      sort(arr, arr + size);
+//      cout << k<<" Largest Element: " << arr[size - k];
+//      return 0;
+//  }
+
+// Check If array is sorted
+// int main()
+// {
+//     vector<int> v;
+//     int input;
+//     while (cin >> input && input != -1)
+//     {
+//         v.push_back(input);
+//     }
+//     bool flag = true;
+//     for (int i = 0; i < v.size()-1; i++)
+//     {
+//         if (v[i] > v[i+1])
+//         {
+//             flag = false;
+//             break;
+//         }
+//     }
+//     cout << flag;
+//     return 0;
+// }
